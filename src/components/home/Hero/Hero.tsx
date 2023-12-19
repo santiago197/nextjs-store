f9a79d2e46976dd7c52e96dce98d37e3c9ff2301
santiago197 <santinacional123.sr@gmail.com>
2023-12-19 16:28:10 -0500

    💄 Implementación Css modules

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,3 +1,10 @@
+import styles from './Hero.module.css';
 export const Hero = () => {
-	return <div>Hero</div>;
+	console.log(styles);
+	return (
+		<section className={styles.Hero}>
+			<h1>Hero</h1>
+			<h2>Batman</h2>
+		</section>
+	);
 };
